7cf67f06db68dddd6326178697ed307244e8486d
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2021-10-31 08:26:51 +0100

    added codes

---
+++ STM32F4 FreeRTOS Template/main.cpp
@@ -0,0 +1,15 @@
+#include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
+#include "FreeRTOS.h"
+#include "task.h"
+
+int main(void)
+{
+	HAL_Init();
+	
+	while(1)
+	{
+
+	}
+}
+
+
